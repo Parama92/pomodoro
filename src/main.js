3d4c2535ec9292dba01546da8282d6d74c12a656
Parama <parama.kar@gmail.com>
2018-05-26 11:40:19 +0530

    Configured vue to make it work with bootstap-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import App from './App'
 import router from './router'
 
+// import 'bootstrap'
+// import 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
