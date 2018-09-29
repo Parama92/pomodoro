e0318e5ee557962b48b253233bea9b2d413b4ad8
Parama92 <parama.kar@gmail.com>
2018-09-29 14:11:24 +0530

    Fixed vulnerablilities and formatted code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import App from './App'
 import router from './router'
-
+/* eslint-disable */
 // import 'bootstrap'
 // import 'bootstrap-vue'
 
